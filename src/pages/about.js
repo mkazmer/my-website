f99d5f276c1c32353e2b750644a9cda9ace759bf
mkazmer <webbasser055@gmail.com>
2019-11-19 22:09:32 -0500

    added first page

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const About = () => (
+  <Layout>
+    <div>
+      <h1>hey here is the about</h1>
+    </div>
+  </Layout>
+)
+
+export default About
