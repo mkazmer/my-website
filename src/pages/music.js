c55b58ed69d48085efb93b833b81e1ea6a4d3214
mkazmer <webbasser055@gmail.com>
2019-11-21 20:47:19 -0500

    adding music page

---
+++ src/pages/music.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const Music = () => (
+  <Layout>
+    <div>
+      <h1>going to put band stuff here</h1>
+    </div>
+  </Layout>
+)
+
+export default Music
