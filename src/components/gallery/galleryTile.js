fb037de581c78427dbed1eeb4d08cb3ee7057db9
mkazmer <webbasser055@gmail.com>
2019-12-01 21:58:25 -0500

    updated images

--- src/components/gallery/galleryTile.js
+++ src/components/gallery/galleryTile.js
@@ -1,6 +1,5 @@
 import React from "react"
 
-import InfoIcon from "../../images/icons/info-icon.png"
 import "./galleryTile.scss"
 
 const GalleryTile = ({ image, toggle, toSelect, setSelected }) => {
@@ -13,7 +12,6 @@ const GalleryTile = ({ image, toggle, toSelect, setSelected }) => {
           setSelected(toSelect)
         }}
       >
-        <img className="info-icon" src={InfoIcon} alt="Info icon" />
         <img className="image" src={image} alt={`${image}`} />
       </button>
     </div>
